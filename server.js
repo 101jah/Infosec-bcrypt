8931fa4074535b75d6e7c5bd516e5749861108d4
101jah <101jah@proton.me>
2024-05-29 08:43:54 -0500

    Updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
-const express     = require('express');
 const bcrypt     = require('bcrypt');
+const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
@@ -12,7 +12,13 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
 
 //END_ASYNC
 
