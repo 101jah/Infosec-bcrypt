97201a61d35e7d1d5528bded0a343933c6560644
101jah <101jah@proton.me>
2024-05-29 09:15:05 -0500

    Updated

--- server.js
+++ server.js
@@ -12,13 +12,10 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-      console.log(res); //true
-    });
-  });
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_ASYNC
 
